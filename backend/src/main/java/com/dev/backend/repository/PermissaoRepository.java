c4f90056cdd0ff42ad25c48db5843a489cfeddf8
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2023-01-04 21:59:12 -0300

    Crud da Permissao

---
+++ backend/src/main/java/com/dev/backend/repository/PermissaoRepository.java
@@ -0,0 +1,8 @@
+package com.dev.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.dev.backend.entity.Permissao;
+
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+
+}
