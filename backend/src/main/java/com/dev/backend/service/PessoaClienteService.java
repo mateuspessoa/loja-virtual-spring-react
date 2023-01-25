f88fe36c9d280ba9cf7f9a6edb870446a8e50fe7
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2023-01-25 13:10:44 -0300

    Envio de email automático com template

--- backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
+++ backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
@@ -1,6 +1,8 @@
 package com.dev.backend.service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,7 +29,11 @@ public class PessoaClienteService {
 		pessoa.setDataCriacao(new Date());
 		Pessoa pessoaNova = pessoaRepository.saveAndFlush(pessoa);
 		permissaoPessoaService.vincularPessoaPermissaoCLiente(pessoaNova);
-		emailService.enviarEmailTexto(pessoaNova.getEmail(), "Cadastro na Loja Teste", "O registro foi realizado com sucesso");
+		//emailService.enviarEmailTexto(pessoaNova.getEmail(), "Cadastro na Loja Teste", "O registro foi realizado com sucesso");
+		Map<String, Object> propMap = new HashMap<>();
+		propMap.put("nome", pessoaNova.getNome());
+		propMap.put("mensagem", "Cadastro Confirmado! Em Breve Você reberá a Sua Senha!");
+		emailService.enviarEmailTemplate(pessoaNova.getEmail(), "Cadastro na Loja Virtual do Mateus", propMap);
 		return pessoaNova;
 	}
 }
